3ee772ceec1585b3f1441089370e05ba1134eb4d
Ehsan Karimi <karimiehsan901@gmail.com>
2022-05-28 17:46:13 +0430

    backend => create project

---
+++ backend/src/test/java/arani/abdollahzade/mohammad/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package arani.abdollahzade.mohammad.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
