65d4c3e3f053783b61941ab384606dd4369419fa
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-05-30 22:19:57 +0430

    data_controller/src/main/java/ir/sahabino/data_collector/serializer/CandleSerializer.java Add CandleSerializer.Class 	Serializer for Candle

---
+++ data_controller/src/main/java/ir/sahabino/data_collector/serializer/CandleSerializer.java
@@ -0,0 +1,25 @@
+package ir.sahabino.data_collector.serializer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ir.sahabino.data_collector.entity.Candle;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class CandleSerializer implements Serializer<Candle> {
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public byte[] serialize(String s, Candle candle) {
+        try {
+            return new ObjectMapper().writer().writeValueAsBytes(candle);
+        } catch (JsonProcessingException e) {
+            throw new SerializationException(e);
+        }
+    }
+}
