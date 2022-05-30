69c1659c01887e0d866e4e53a6906c399a98474b
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-05-30 23:00:14 +0430

    rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/entity/Candle.java Create Candle.Class 	1) Add Jakson Annotation 	2) Hibernate Annotation

---
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/entity/Candle.java
@@ -0,0 +1,87 @@
+package ir.sahabino.rules_evaluator.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Candle{
+
+    @JsonProperty("open_time")
+    private Long openTime;
+    @JsonProperty("open")
+    private String open;
+    @JsonProperty("high")
+    private String high;
+    @JsonProperty("low")
+    private String low;
+    @JsonProperty("close")
+    private String close;
+    @JsonProperty("volume")
+    private String volume;
+    @JsonProperty("close_time")
+    private Long closeTime;
+    @JsonProperty("quote_asset_volume")
+    private String quoteAssetVolume;
+    @JsonProperty("number_of_trades")
+    private Long numberOfTrades;
+    @JsonProperty("taker_buy_base_asset_volume")
+    private String takerBuyBaseAssetVolume;
+    @JsonProperty("taker_buy_quote_asset_volume")
+    private String takerBuyQuoteAssetVolume;
+
+    private Candle(String open, String high, String low, String close) {
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+    }
+
+    public Candle() {
+    }
+
+    public static Candle build(String open, String high, String low, String close) {
+        return new Candle(open, high, low, close);
+    }
+
+    public Candle openTime(Long openTime) {
+        this.openTime = openTime;
+        return this;
+    }
+
+    public Candle volume(String volume) {
+        this.volume = volume;
+        return this;
+    }
+
+    public Candle closeTime(Long closeTime) {
+        this.closeTime = closeTime;
+        return this;
+    }
+
+    public Candle quoteAssetVolume(String quoteAssetVolume) {
+        this.quoteAssetVolume = quoteAssetVolume;
+        return this;
+    }
+
+    public Candle numberOfTrades(Long numberOfTrades) {
+        this.numberOfTrades = numberOfTrades;
+        return this;
+    }
+
+    public Candle takerBuyBaseAssetVolume(String takerBuyBaseAssetVolume) {
+        this.takerBuyBaseAssetVolume = takerBuyBaseAssetVolume;
+        return this;
+    }
+
+    public Candle takerBuyQuoteAssetVolume(String takerBuyQuoteAssetVolume) {
+        this.takerBuyQuoteAssetVolume = takerBuyQuoteAssetVolume;
+        return this;
+    }
+
+}
+
