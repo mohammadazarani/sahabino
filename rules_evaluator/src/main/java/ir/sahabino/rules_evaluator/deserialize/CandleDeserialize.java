adabd1635d594e9e10188bfb231a67c8c15b8869
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-05-31 19:08:48 +0430

    rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/deserialize/CandleDeserialize.java CREATE CandleDeserialize.Class 	using ObjectMapper to convert byte[] to Candle.Class

---
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/deserialize/CandleDeserialize.java
@@ -0,0 +1,43 @@
+package ir.sahabino.rules_evaluator.deserialize;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ir.sahabino.rules_evaluator.entity.Candle;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class CandleDeserialize implements Deserializer<Candle> {
+//    protected Deserializer<List<Candle>> kafkaDeserializer(String json) {
+//        try {
+//            new ObjectMapper().readValue(json, Candle.class);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//        ObjectMapper om = new ObjectMapper();
+//        om.getTypeFactory().constructParametricType(List.class, Candle.class);
+//        return new JsonDeserializer<>(om);
+//    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public Candle deserialize(String s, byte[] bytes) {
+        try {
+            return new ObjectMapper().reader().readValue(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        return new ObjectMapper().writer().writeValueAsBytes(candle);
+
+//        Item itemWithOwner = new ObjectMapper().readValue(json, Item.class);
+
+        return null;
+    }
+}
