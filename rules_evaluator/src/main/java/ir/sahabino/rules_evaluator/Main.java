fbe52719de4afa44e3c2886478bf8de8d89dd85a
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-05-31 22:14:02 +0430

    rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/Main.java Change Consuming way: 	using while true to solve kafka rebalancing

--- rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/Main.java
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/Main.java
@@ -3,10 +3,9 @@ package ir.sahabino.rules_evaluator;
 import ir.sahabino.rules_evaluator.conf.Conf;
 import ir.sahabino.rules_evaluator.dao.KafKaCandleConsumer;
 import ir.sahabino.rules_evaluator.factory.KafkaConsumerFactory;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-
-import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +13,15 @@ public class Main {
         KafkaConsumer<String, String> kafkaConsumer = KafkaConsumerFactory.createKafkaConsumer(config);
 
         KafKaCandleConsumer kafKaCandleConsumer = KafKaCandleConsumer.of(config, kafkaConsumer);
-        kafKaCandleConsumer.consume().forEach(System.out::println);
+        while (true) {
+            ConsumerRecords<String, String> records = kafKaCandleConsumer.consume();
 
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.value());
+                System.out.println(record.key());
+            }
+            System.out.println(records.count());
+
+        }
     }
 }
