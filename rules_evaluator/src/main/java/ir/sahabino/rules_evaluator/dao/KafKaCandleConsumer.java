2047947075e5316c31db776b92bb67d14852d460
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-05-31 17:21:15 +0430

    rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/dao/KafKaCandleConsumer.java Change    KafKaCandleConsumer Constructor Add       ConsumerRecords<String, String> consume.Method

--- rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/dao/KafKaCandleConsumer.java
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/dao/KafKaCandleConsumer.java
@@ -1,15 +1,27 @@
 package ir.sahabino.rules_evaluator.dao;
 
 import ir.sahabino.rules_evaluator.conf.Conf;
+import ir.sahabino.rules_evaluator.entity.Candle;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
 
 public class KafKaCandleConsumer {
     private Conf config;
+    private KafkaConsumer<String, String> kafkaConsumer;
 
-    private KafKaCandleConsumer(Conf config) {
+    private KafKaCandleConsumer(Conf config, KafkaConsumer<String, String> kafkaConsumer) {
         this.config = config;
+        this.kafkaConsumer = kafkaConsumer;
     }
 
-    public static KafKaCandleConsumer of(Conf config) {
-        return new KafKaCandleConsumer(config);
+    public static KafKaCandleConsumer of(Conf config, KafkaConsumer<String, String> kafkaConsumer) {
+        return new KafKaCandleConsumer(config, kafkaConsumer);
     }
+
+    public ConsumerRecords<String, String> consume() {
+        return kafkaConsumer.poll(Duration.ofMillis(1000));
+    }
+
 }
