69ab62a24eb0fb1de74e11591998eb6254fb2ba0
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 17:53:43 +0430

    data_controller/src/main/java/ir/sahabino/data_collector/collector/ Add non-default constructor to ETH and BTC

--- data_controller/src/main/java/ir/sahabino/data_collector/collector/BTCCollector.java
+++ data_controller/src/main/java/ir/sahabino/data_collector/collector/BTCCollector.java
@@ -1,5 +1,10 @@
 package ir.sahabino.data_collector.collector;
 
+import java.util.Properties;
+
 public class BTCCollector extends Collector {
 
+    public BTCCollector(Properties properties) {
+        super(properties);
+    }
 }
--- data_controller/src/main/java/ir/sahabino/data_collector/collector/ETHCollector.java
+++ data_controller/src/main/java/ir/sahabino/data_collector/collector/ETHCollector.java
@@ -1,5 +1,10 @@
 package ir.sahabino.data_collector.collector;
 
+import java.util.Properties;
+
 public class ETHCollector extends Collector {
 
+    public ETHCollector(Properties properties) {
+        super(properties);
+    }
 }
