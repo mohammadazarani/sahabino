527698523842033108663a08cf0ed0a317eeb912
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 17:45:47 +0430

    data_controller/src/main/java/ir/sahabino/data_collector/collector/Collector.java Add KafkaCandleProducer to collect.Method

--- data_controller/src/main/java/ir/sahabino/data_collector/collector/Collector.java
+++ data_controller/src/main/java/ir/sahabino/data_collector/collector/Collector.java
@@ -5,25 +5,38 @@ import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.market.Candlestick;
 import com.binance.api.client.domain.market.CandlestickInterval;
 import ir.sahabino.data_collector.conf.CollectorConfigValues;
+import ir.sahabino.data_collector.conf.KafkaConf;
+import ir.sahabino.data_collector.dao.KafkaCandleProducer;
 import ir.sahabino.data_collector.entity.Candle;
+import ir.sahabino.data_collector.factory.KafkaProducerFactory;
+import org.apache.kafka.clients.producer.KafkaProducer;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class Collector {
     private final Properties properties;
 
+    private final BinanceApiRestClient client;
+    private List<Candlestick> candlestickBars;
+    private final KafkaCandleProducer kafkaCandleProducer;
 
     public Collector(Properties properties) {
+        client = BinanceApiClientFactory.newInstance().newRestClient();
+        candlestickBars = new ArrayList<>();
+        KafkaConf config = KafkaConf.load();
+        KafkaProducer<String, Candle> kafkaProducer = KafkaProducerFactory.createKafkaProducer(config);
+        kafkaCandleProducer = KafkaCandleProducer.of(config, kafkaProducer);
         this.properties = properties;
     }
 
+
     public List<Candle> collect() {
-        BinanceApiRestClient client = BinanceApiClientFactory.newInstance().newRestClient();
-        List<Candlestick> candlestickBars = client.
+        candlestickBars = client.
                 getCandlestickBars(properties.getProperty(CollectorConfigValues.BINACE_MARKETS), CandlestickInterval.ONE_MINUTE);
 
-//        candlestickBars.forEach(System.out::println);
+
         for (Candlestick candlestickBar : candlestickBars) {
             System.out.println(candlestickBar);
             kafkaCandleProducer.produce(
