e76522465cfbdb6bf33476081a850349def761e2
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 14:07:38 +0430

    backend/src/main/java/arani/abdollahzade/mohammad/backend/service/CandleService.java Add CandleRepository(Autowired)

--- backend/src/main/java/arani/abdollahzade/mohammad/backend/service/CandleService.java
+++ backend/src/main/java/arani/abdollahzade/mohammad/backend/service/CandleService.java
@@ -1,13 +1,22 @@
 package arani.abdollahzade.mohammad.backend.service;
 
 import arani.abdollahzade.mohammad.backend.entity.Candle;
+import arani.abdollahzade.mohammad.backend.repository.CandleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CandleService {
-    public List<Candle> getCandles(){
+    private final CandleRepository candleRepository;
 
+    @Autowired
+    public CandleService(CandleRepository candleRepository) {
+        this.candleRepository = candleRepository;
+    }
+
+    public List<Candle> getCandles(){
+        return candleRepository.findAll();
     }
 }
