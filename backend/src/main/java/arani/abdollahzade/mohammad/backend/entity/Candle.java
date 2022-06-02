af2496a4266afcb2df530544b430434bbfd2a1b7
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 12:30:45 +0430

    backend/src/main/java/arani/abdollahzade/mohammad/backend/entity/Candle.java Add data JPA annotaions for Candle.Class 	@Entitu 	@Table 	@GenerateID

--- backend/src/main/java/arani/abdollahzade/mohammad/backend/entity/Candle.java
+++ backend/src/main/java/arani/abdollahzade/mohammad/backend/entity/Candle.java
@@ -4,12 +4,23 @@ package arani.abdollahzade.mohammad.backend.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
+@Table
 public class Candle {
+    @Id
+    @SequenceGenerator(
+            name = "candle_sequence",
+            sequenceName = "candle_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
 
     private Long openTime;
