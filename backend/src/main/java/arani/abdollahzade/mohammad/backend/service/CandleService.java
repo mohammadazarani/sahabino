dbce5680828884dd71d2baae24485c802dcecdb0
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 10:55:07 +0430

    backend/src/main/java/arani/abdollahzade/mohammad/backend/service/CandleService.java CREATE CandleService: 	to get candles from database

---
+++ backend/src/main/java/arani/abdollahzade/mohammad/backend/service/CandleService.java
@@ -0,0 +1,13 @@
+package arani.abdollahzade.mohammad.backend.service;
+
+import arani.abdollahzade.mohammad.backend.entity.Candle;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CandleService {
+    public List<Candle> getCandles(){
+
+    }
+}
