71b9e39dcfad17acfe8408420121958e77af97e8
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 16:56:39 +0430

    data_controller/src/main/java/ir/sahabino/data_collector/dao/KafkaCandleProducer.java Rename

--- data_controller/src/main/java/ir/sahabino/data_collector/dao/KafkaCandleProducer.java
+++ data_controller/src/main/java/ir/sahabino/data_collector/dao/KafkaCandleProducer.java
@@ -1,6 +1,6 @@
 package ir.sahabino.data_collector.dao;
 
-import ir.sahabino.data_collector.conf.Conf;
+import ir.sahabino.data_collector.conf.KafkaConf;
 import ir.sahabino.data_collector.entity.Candle;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -8,16 +8,16 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class KafkaCandleProducer {
-    private Conf config;
+    private KafkaConf config;
     private KafkaProducer<String, Candle> kafkaProducer;
     private AtomicInteger candlesCount = new AtomicInteger(0);
 
-    private KafkaCandleProducer(Conf config, KafkaProducer<String, Candle> kafkaProducer) {
+    private KafkaCandleProducer(KafkaConf config, KafkaProducer<String, Candle> kafkaProducer) {
         this.config = config;
         this.kafkaProducer = kafkaProducer;
     }
 
-    public static KafkaCandleProducer of(Conf config, KafkaProducer<String, Candle> kafkaProducer){
+    public static KafkaCandleProducer of(KafkaConf config, KafkaProducer<String, Candle> kafkaProducer){
         return new KafkaCandleProducer(config, kafkaProducer);
     }
 
