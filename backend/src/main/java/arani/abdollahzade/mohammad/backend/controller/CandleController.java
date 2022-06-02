7ad50b2595d559c5ad6efc238373f434bccf955d
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-02 10:56:36 +0430

    backend/src/main/java/arani/abdollahzade/mohammad/backend/controller/CandleController.java CREATE controller: 	getting needed result from CandleService 	Autowired CandleService

---
+++ backend/src/main/java/arani/abdollahzade/mohammad/backend/controller/CandleController.java
@@ -0,0 +1,25 @@
+package arani.abdollahzade.mohammad.backend.controller;
+
+
+import arani.abdollahzade.mohammad.backend.entity.Candle;
+import arani.abdollahzade.mohammad.backend.service.CandleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/candles")
+public class CandleController {
+    private final CandleService candleService;
+
+    @Autowired
+    public CandleController(CandleService candleService) {
+        this.candleService = candleService;
+    }
+
+    public List<Candle> getCandles() {
+        return candleService.getCandles();
+    }
+}
