c96b3e88a31d1a3e5a48fb850e77a2dbe3b6cb98
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-03 17:48:06 +0430

    Add KafkaStreamFactory write topology

--- rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/factory/KafkaStreamFactory.java
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/factory/KafkaStreamFactory.java
@@ -1,37 +1,41 @@
 package ir.sahabino.rules_evaluator.factory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ir.sahabino.rules_evaluator.conf.Conf;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import ir.sahabino.rules_evaluator.entity.Candle;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 
-import java.util.Collections;
 import java.util.Properties;
 
 public class KafkaStreamFactory {
 
-    public static KStream<String, String> createKafkaStream(Conf conf) {
+    public static KafkaStreams createKafkaStream(Conf conf) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, conf.getKafkaBrokers());
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         final StreamsBuilder builder = new StreamsBuilder();
+        final ObjectMapper objectMapper = new ObjectMapper();
 
-        KStream<String, String> source = builder.stream(conf.getKafkaTopic());
-//        source.filter(ca)
-//        source.filter();
-//        KafkaStreams k  = new KafkaStreams(conf.getKafkaTopic(), props);
+        KStream<String, String> source = builder.stream(conf.getKafkaTopic(),
+                Consumed.with(Serdes.String(), Serdes.String()));
 
-        return source;
-//        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-
-//        consumer.subscribe(Collections.singletonList(conf.getKafkaTopic()));
-//        return consumer;
-
+        source.map((k, v) -> {
+            try {
+                Candle candle = objectMapper.readValue(v, Candle.class);
+                return new KeyValue<>(k, candle);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            return new KeyValue<>(k, v);
+        }).filter((k, v) -> ((Candle)v).getCloseTime() > 1000000 );
+        final Topology topology = builder.build();
+        return new KafkaStreams(topology, props);
 
     }
 }
