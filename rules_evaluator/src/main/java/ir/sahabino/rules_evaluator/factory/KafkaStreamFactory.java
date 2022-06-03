00fe8931f5e2a7a5a90e0934211986a75040b18b
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-03 18:39:47 +0430

    New Stream builder, Test it

--- rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/factory/KafkaStreamFactory.java
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/factory/KafkaStreamFactory.java
@@ -15,7 +15,7 @@ public class KafkaStreamFactory {
 
     public static KafkaStreams createKafkaStream(Conf conf) {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe2");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, conf.getKafkaBrokers());
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -25,16 +25,23 @@ public class KafkaStreamFactory {
         KStream<String, String> source = builder.stream(conf.getKafkaTopic(),
                 Consumed.with(Serdes.String(), Serdes.String()));
 
-        source.map((k, v) -> {
-            try {
-                Candle candle = objectMapper.readValue(v, Candle.class);
-                return new KeyValue<>(k, candle);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            return new KeyValue<>(k, v);
-        }).filter((k, v) -> ((Candle)v).getCloseTime() > 1000000 );
+        //####
+        System.out.println(source);
+
+//        source.map((k, v) -> {
+//            try {
+//                Candle candle = objectMapper.readValue(v, Candle.class);
+//                return new KeyValue<>(k, candle);
+//            } catch (JsonProcessingException e) {
+//                e.printStackTrace();
+//            }
+//            return new KeyValue<>(k, v);
+//        }).filter((k, v) -> ((Candle)v).getCloseTime() > 1000L ).to("asghar");
+        source.to("holy");
         final Topology topology = builder.build();
+
+        //####
+        System.out.println(topology.describe());
         return new KafkaStreams(topology, props);
 
     }
