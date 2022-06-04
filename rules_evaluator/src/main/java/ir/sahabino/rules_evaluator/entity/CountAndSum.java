4fa8fa3402f5a6c3d1deb8678bd544ac6fbe1ff6
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-04 17:02:47 +0430

    Create CountAndSum.Class to count and summerizing kafka Candle close value in agg

---
+++ rules_evaluator/src/main/java/ir/sahabino/rules_evaluator/entity/CountAndSum.java
@@ -0,0 +1,12 @@
+package ir.sahabino.rules_evaluator.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CountAndSum {
+    private Long sum;
+    private Long count;
+
+}
