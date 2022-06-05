dbc29f657f143e029c267adc2cbf100c31c266b5
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-05 12:18:40 +0430

    Add request_period for collector

--- data_controller/src/main/java/ir/sahabino/data_collector/conf/CollectorConfigValues.java
+++ data_controller/src/main/java/ir/sahabino/data_collector/conf/CollectorConfigValues.java
@@ -2,5 +2,6 @@ package ir.sahabino.data_collector.conf;
 
 public class CollectorConfigValues {
     public static String BINACE_MARKETS = "markets";
+    public static String REQUEST_PERIOD = "request_period";
 
 }
