52f862876c89576648ec3cecc29a103e71432ac9
Mohammad Abdollahzade <mamadazarani@gmail.com>
2022-06-05 16:07:54 +0430

    Add Test for NoSuchFieldException in Indictor get Field Method

--- rules_evaluator/src/test/java/ir/sahabino/rules_evaluator/rules/IndicatorTest.java
+++ rules_evaluator/src/test/java/ir/sahabino/rules_evaluator/rules/IndicatorTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class IndicatorTest {
 
@@ -20,5 +21,15 @@ public class IndicatorTest {
         method.setAccessible(true);
         Method close = (Method) method.invoke(indicator, "close");
         assertEquals(close.getName(), "getClose");
+    }
+//    @Test(expected = NoSuchRuleFieldExcetion.class)
+    @Test
+    public void getNotExistenceField() throws NoSuchMethodException {
+        Indicator indicator = new Indicator();
+        Method method = Indicator.class.getDeclaredMethod("getFieldToOperateOn", String.class);
+        method.setAccessible(true);
+        Exception exception = assertThrows(InvocationTargetException.class, () -> {
+            method.invoke(indicator, "clos");
+        });
     }
 }
